ae3fa5665aad8262482e48cb55121ebaf3574a3c
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-06-22 18:29:31 +0200

    Revert "bug"
    This reverts commit a6978123624ce8d85ceda92756e5672aef37848f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,10 @@ import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,HttpClientModule,],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
